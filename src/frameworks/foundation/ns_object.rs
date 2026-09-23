3cec99c50f8b4240710edac763c5665d60cb2e19
touchHLE builder <builder@localhost>
2026-09-23 16:32:45 +0300

    Remove invalid methodForSelector return type

--- src/frameworks/foundation/ns_object.rs
+++ src/frameworks/foundation/ns_object.rs
@@ -13,9 +13,9 @@
 //!   explains the algorithm `setValue:forKey:` should follow.
 //!
 //! See also: [crate::objc], especially the `objects` module.
+
 use super::ns_string::{from_rust_string, to_rust_string};
 use super::{NSTimeInterval, NSUInteger};
-use crate::abi::GuestFunction;
 use crate::frameworks::foundation::ns_run_loop::{add_perform_request, cancel_perform_requests};
 use crate::frameworks::foundation::ns_thread::detach_new_thread_inner;
 use crate::libc::semaphore::{host_destroy_semaphore, sem_wait};
@@ -65,10 +65,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 + (bool)instancesRespondToSelector:(SEL)selector {
     env.objc.class_has_method(this, selector)
 }
-+ (GuestFunction)methodForSelector:(SEL)selector {
-    env.objc.class_get_guest_method_implementation(this, selector)
-        .unwrap_or(GuestFunction::from_addr_with_thumb_bit(0))
-}
+
 + (())cancelPreviousPerformRequestsWithTarget:(id)target selector:(SEL)selector object:(id)arg {
     let run_loop: id = msg_class![env; NSRunLoop currentRunLoop];
     cancel_perform_requests(env, run_loop, target, selector, arg);
